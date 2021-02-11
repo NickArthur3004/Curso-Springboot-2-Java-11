e5583cea5fb119011dd2a2cd79031b95df7c6433
NickArthur3004 <nicolas.arthur2786@gmail.com>
2021-02-11 10:43:33 -0300

    OrderItem, many-to-many association with extra attributes PART2

--- src/main/java/com/example/Spring/Entities/Order.java
+++ src/main/java/com/example/Spring/Entities/Order.java
@@ -2,6 +2,8 @@ package com.example.Spring.Entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.example.Spring.Entities.enums.OrderStatus;
@@ -30,7 +33,8 @@ public class Order implements Serializable{
 	
 	private Integer orderStatus;
 	
-	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	@ManyToOne
 	@JoinColumn(name = "client_Id")
@@ -77,6 +81,10 @@ public class Order implements Serializable{
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	public Set<OrderItem> getItems(){
+		return items;
 	}
 
 	@Override
